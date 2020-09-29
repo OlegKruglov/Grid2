db2454812db8e4cfe8f64e14b087727e2acd19e6
алекс <@DESKTOP-TVQ711I>
2020-09-30 01:17:53 +0300

    Добавьте файлы проекта.

---
+++ Grid2/Grid2/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Grid2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var boxTapHandler = new TapGestureRecognizer();
+            boxTapHandler.Tapped += ChangeColor;
+            myBox.GestureRecognizers.Add(boxTapHandler);
+        }
+
+        private void ChangeColor(object sender, EventArgs args)
+        {
+            myBox.Color = Color.Red;
+        }
+    }
+}
